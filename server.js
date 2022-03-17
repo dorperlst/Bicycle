ee82f0acc441ce9bd0026b6c06d933616a289bc2
doronps <dorperlst@gmail.com>
2022-03-17 14:58:40 +0200

    Prepare for deploy

--- server.js
+++ server.js
@@ -18,5 +18,6 @@ if(process.env.NODE_ENV === 'production')
     app.use(express.static("client/build"))
     app.get('*',(req,res)=>res.sendFile(path.resolve)(__dirname,"client", "build","index.html"));
 }
-const PORT = process.env.port || 5000
+const PORT = 5000
+PORT = process.env.port || 5000
 app.listen(PORT,()=>console.log(`server start on port ${PORT}`))
